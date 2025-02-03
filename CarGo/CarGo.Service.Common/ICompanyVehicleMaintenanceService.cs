fab9a5755e14c46efbf6b1b1036341c1c80acb75
filip7557 <filip.curic@student.ferit.hr>
2025-02-03 13:44:03 +0100

    Code cleanup

--- CarGo/CarGo.Service.Common/ICompanyVehicleMaintenanceService.cs
+++ CarGo/CarGo.Service.Common/ICompanyVehicleMaintenanceService.cs
@@ -1,9 +1,11 @@
-﻿using CarGo.Model;
+﻿using CarGo.Common;
+using CarGo.Model;
 
 namespace CarGo.Service.Common
 {
     public interface ICompanyVehicleMaintenanceService
     {
         public Task<bool> SaveCompanyVehicleMaintenanceAsync(CompanyVehicleMaintenance maintenance, Guid createdByUserId);
+        public Task<PagedResponse<CompanyVehicleMaintenance>> GetMaintenancesByCompanyVehicleIdAsync(Guid companyVehicleId, Paging paging);
     }
 }
