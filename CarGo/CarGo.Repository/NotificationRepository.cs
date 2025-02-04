84d8cd67aba2a2d086f2d24148ab8466d72305d0
filip7557 <filip.curic@student.ferit.hr>
2025-02-04 12:47:09 +0100

    Notifications repository

--- CarGo/CarGo.Repository/NotificationRepository.cs
+++ CarGo/CarGo.Repository/NotificationRepository.cs
@@ -1,8 +1,28 @@
-﻿using CarGo.Repository.Common;
+﻿using CarGo.Model;
+using CarGo.Repository.Common;
 
 namespace CarGo.Repository
 {
     public class NotificationRepository : INotificationRepository
     {
+        private string? _connectionString;
+
+        public NotificationRepository()
+        {
+            _connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__PostgresDb")
+            ?? throw new InvalidOperationException("Database connection string is not set.");
+        }
+        public async Task<bool> SaveNotificationAsync(Notification notification)
+        {
+            try
+            {
+                using (var connection )
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception:" + ex.Message);
+                return false;
+            }
+        }
     }
 }
