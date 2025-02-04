ddd1fa574cedf5bc7ea2ac41285a525029d425b6
filip7557 <filip.curic@student.ferit.hr>
2025-02-04 12:34:46 +0100

    Add notifications

---
+++ CarGo/CarGo.Model/Notification.cs
@@ -0,0 +1,12 @@
+﻿namespace CarGo.Model
+{
+    public class Notification
+    {
+        public required Guid Id { get; set; }
+        public required string Title { get; set; }
+        public string? Text { get; set; }
+        public required Guid BookingId { get; set; }
+        public string? From { get; set; }
+        public string? To { get; set; }
+    }
+}
---
+++ CarGo/CarGo.Repository.Common/INotificationRepository.cs
@@ -0,0 +1,14 @@
+﻿using CarGo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarGo.Repository.Common
+{
+    public interface INotificationRepository
+    {
+        public Task<bool> SaveNotificationAsync(Notification notification);
+    }
+}
