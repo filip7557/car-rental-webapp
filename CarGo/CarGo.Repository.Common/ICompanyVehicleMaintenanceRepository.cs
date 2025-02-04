4013672810af889a98d8f259f7dc2192b6822243
filip7557 <filip.curic@student.ferit.hr>
2025-02-04 12:36:01 +0100

    Fix params

--- CarGo/CarGo.Repository.Common/ICompanyVehicleMaintenanceRepository.cs
+++ CarGo/CarGo.Repository.Common/ICompanyVehicleMaintenanceRepository.cs
@@ -7,9 +7,9 @@ namespace CarGo.Repository.Common
     {
         public Task<bool> SaveCompanyVehicleMaintenanceAsync(CompanyVehicleMaintenance maintenance, Guid createdByUserId);
 
-        public Task<bool> DeleteCompanyVehicleMaintenanceByIdAsync(Guid maintenanceId, Guid userId, bool isActiveFilter);
+        public Task<bool> DeleteCompanyVehicleMaintenanceByIdAsync(Guid maintenanceId, Guid userId);
 
-        public Task<List<CompanyVehicleMaintenance>> GetMaintenancesByCompanyVehicleIdAsync(Guid companyVehicleId, Paging paging);
+        public Task<List<CompanyVehicleMaintenance>> GetMaintenancesByCompanyVehicleIdAsync(Guid companyVehicleId, Paging paging, bool isActiveFilter);
 
         public Task<int> CountAsync(Guid companyVehicleId);
     }
