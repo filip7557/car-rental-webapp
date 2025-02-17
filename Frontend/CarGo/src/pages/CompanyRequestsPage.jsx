1571a4d3ddfe3ce2a5dc7f230c0f1b0d0ce6cd9a
Sandro Savi <sandro.savi0088@gmail.com>
2025-02-17 15:00:39 +0100

    ﻿Manage company requests created

---
+++ Frontend/CarGo/src/pages/CompanyRequestsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CompanyRequests from "../components/CompanyRequests";
+
+const CompanyRequestsPage = () => {
+  return (
+    <div>
+      <h1>Manage Company Requests</h1>
+      <CompanyRequests />
+    </div>
+  );
+};
+
+export default CompanyRequestsPage;
