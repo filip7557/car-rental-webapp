2377ee27d347e6bc480bced55175e25695eec4a1
filip7557 <filip.curic@student.ferit.hr>
2025-02-17 14:24:20 +0100

    Add homepage

--- Frontend/CarGo/src/App.jsx
+++ Frontend/CarGo/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 
 import RegisterPage from './pages/RegisterPage/RegisterPage'
 import LoginPage from './pages/LoginPage/LoginPage'
+import HomePage from './pages/HomePage/HomePage'
 
 function App() {
   return (
     <Routes>
       <Route path='register' element={<RegisterPage />} />
       <Route path='login' element={<LoginPage />} />
+      <Route path='/' element={<HomePage />} />
     </Routes>
   )
 }
