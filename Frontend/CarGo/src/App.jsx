3dc4900e6f2e95f079fa8c8bae7808774b8c0ec1
Davigula <ddujmov123@gmail.com>
2025-02-17 15:13:56 +0100

    ﻿form

--- Frontend/CarGo/src/App.jsx
+++ Frontend/CarGo/src/App.jsx
@@ -1,33 +1,33 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import HomePage from "./pages/HomePage/HomePage";
-import BookingsPage from "./pages/BookingsPage/BookingsPage";
-import ProfilePage from "./pages/ProfilePage/ProfilePage";
-import EditProfilePage from "./pages/EditProfilePage/EditProfilePage";
-import AddVehiclePage from "./pages/VehiclePage/AddVehiclePage.jsx";
-import CompanyRequestsPage from "./pages/CompanyRequestsPage/CompanyRequestsPage.jsx";
-import CompanyRegisterPage from "./pages/CompanyRegisterPage/CompanyRegisterPage.jsx";
 import AddDamageReportPage from "./pages/AddDamageReportPage/AddDamageReportPage.jsx";
+import BookingsPage from "./pages/BookingsPage/BookingsPage";
+import CompanyRegisterPage from "./pages/CompanyRegisterPage/CompanyRegisterPage.jsx";
+import CompanyRequestsPage from "./pages/CompanyRequestsPage/CompanyRequestsPage.jsx";
+import EditProfilePage from "./pages/EditProfilePage/EditProfilePage";
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import AddVehiclePage from "./pages/VehiclePage/AddVehiclePage.jsx";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="register" element={<RegisterPage />} />
-      <Route path="login" element={<LoginPage />} />
-      <Route path="/" element={<HomePage />} />
-      <Route path="home" element={<HomePage />} />
-      <Route path="profile" element={<ProfilePage />} />
-      <Route path="profile/:id" element={<EditProfilePage />} />
-      <Route path="/add-vehicle" element={<AddVehiclePage />} />
-      <Route path="/company-register" element={<CompanyRegisterPage />} />
-      <Route path="/company-requests" element={<CompanyRequestsPage />} />
-      <Route path="/addDamageReport/:id" element={<AddDamageReportPage />} />
-      <Route path="/bookingsPage" element={<BookingsPage />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="register" element={<RegisterPage />} />
+			<Route path="login" element={<LoginPage />} />
+			<Route path="/" element={<HomePage />} />
+			<Route path="home" element={<HomePage />} />
+			<Route path="profile" element={<ProfilePage />} />
+			<Route path="profile/:id" element={<EditProfilePage />} />
+			<Route path="/add-vehicle" element={<AddVehiclePage />} />
+			<Route path="/company-register" element={<CompanyRegisterPage />} />
+			<Route path="/company-requests" element={<CompanyRequestsPage />} />
+			<Route path="/addDamageReport/:id" element={<AddDamageReportPage />} />
+			<Route path="/bookingsPage" element={<BookingsPage />} />
+		</Routes>
+	);
 }
 
 export default App;
---
+++ Frontend/CarGo/src/Components/NavBar/Form.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./NavBar";
+
+function Form() {
+	return (
+		<div>
+			<NavBar />
+			<div></div>
+		</div>
+	);
+}
+
+export default Form;
